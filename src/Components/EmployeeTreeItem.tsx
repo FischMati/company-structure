0538f6ceb1e52c24eed40821a17fef1d3fc0a453
FischerMatias <mfischerprofesional@gmail.com>
2019-10-22 10:36:53 -0400

    one button with loader ready

--- src/Components/EmployeeTreeItem.tsx
+++ src/Components/EmployeeTreeItem.tsx
@@ -1,26 +1,80 @@
-import React from "react";
-import "./App.css";
-import logo from "./logo.svg";
+import React, {useState} from "react";
+import { Button } from "reactstrap";
+import styled from "styled-components";
 
-const App: React.FC = () => {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
-};
+const EmployeeButton = styled(Button)`
+    position: relative;
+    max-width: 500px;
+      
+      &:before {
+        content: '';
+        position: absolute;
+        right: 16px;
+        top: 50%;
+        margin-top: -12px;
+        width: 24px;
+        height: 24px;
+        border: 2px solid;
+        border-left-color: transparent;
+        border-right-color: transparent;
+        border-radius: 50%;
+        opacity: 0;
+        transition: opacity 0.5s;
+        animation: 0.8s linear infinite rotate;
+      }
+      &.sending{
+        pointer-events: none;
+        cursor: not-allowed;
+        
+        &:before {
+          transition-delay: 0.5s;
+          transition-duration: 1s;
+          opacity: 1;
+        }
+      }
+    }
+    
+    @keyframes rotate {
+      0% {
+        transform: rotate(0deg);
+      }
+      100% {
+        transform: rotate(360deg);
+      }
+    }
+    
+    @keyframes fade {
+      0%   { opacity: 0.5; }
+      100% { opacity: 1.0; }
+    }
+`;
 
-export default App;
+const EmployeeTreeitem =
+    () => {
+        const [btnState, setBtnState] = useState("");
+
+        const onClick = (e: Event) => {
+            e.preventDefault();
+
+            setBtnState("sending");
+
+            setTimeout(() =>
+                setBtnState("")
+            , 4500);
+        };
+
+        return (
+            <EmployeeButton
+                outline
+                color="primary"
+                size="lg"
+                onClick={onClick}
+                className={btnState}
+                block
+            >
+                Click me
+            </EmployeeButton>
+        );
+    };
+
+export default EmployeeTreeitem;
